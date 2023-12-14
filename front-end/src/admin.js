d2065a1540d9ba302522c11d3f8ca6f1dd3d2d73
1155144508 <1155144508@link.cuhk.edu.hk>
2023-12-15 03:12:38 +0800

    Update admin.js, indes.js, login.js and server.js
    Admin page can now read all the accounts.

--- front-end/src/admin.js
+++ front-end/src/admin.js
@@ -27,26 +27,26 @@ const fakeData_Event = [{"ID":1,"name":"Errol","price":32,"quota":45},
 {"ID":10,"name":"Glenn","price":86,"quota":74}]
 
 export default class Admin extends React.Component{
-
-
     render() {
-       return (<body style={{ backgroundColor: "#FBE8A6", minHeight: "100vh"}}>
-                <div className='row' style={{width: "100%"}}>
-                  <Logout logout={this.props.logout}/>
-                  <div className='col-sm-1'></div>
-                  <h1 className='col-sm-8' style={{textAlign: "center", marginTop: 0}}>{title}</h1>
-                </div>
-                <h1 style={{width: "100%", textAlign: "center", marginTop: 30}}>Admin Dashboard</h1>
-                <br />
-                  <NavTable />
-              </body>
-    )}
+       return (
+        <body style={{ backgroundColor: "#FBE8A6", minHeight: "100vh"}}>
+          <div className='row' style={{width: "100%"}}>
+            <Logout onLogout={this.props.onLogout}/>
+            <div className='col-sm-1'></div>
+            <h1 className='col-sm-8' style={{textAlign: "center", marginTop: 0}}>{title}</h1>
+          </div>
+          <h1 style={{width: "100%", textAlign: "center", marginTop: 30}}>Admin Dashboard</h1>
+          <br />
+          <NavTable />
+        </body>
+      )
+    }
 }
 
 class Logout extends React.Component{
   render() {
     return (
-      <button className='col-sm-1 btn btn-primary' style={{backgroundColor: "#303C6C"}} onClick={() => this.props.logout(false, undefined)}>Logout</button>
+      <button className='col-sm-1 btn btn-primary' style={{backgroundColor: "#303C6C"}} onClick={() => this.props.onLogout()}>Logout</button>
     )
   }
 }
@@ -59,19 +59,17 @@ class NavTable extends React.Component{
   render() {
     return (
       <div className='container'>
-      <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "#D2FDFF"}}>
-        <div className='collapse navbar-collapse'>
-          <ul className='navbar-nav'>
-            <li className='nav-item active'>
-              <a className='nav-link' href="#" style={this.state.viewEvent?{fontWeight: "bolder", color: "Highlight"}:{fontWeight: "bolder"}} onClick={() => this.setState({viewEvent: true})}>Events</a>
-            </li>
-            <li className='nav-item active' style={{marginLeft: "10vw"}}>
-              <a className='nav-link' href="#" style={!this.state.viewEvent?{fontWeight: "bolder", color: "Highlight"}:{fontWeight: "bolder"}} onClick={() => this.setState({viewEvent: false})}>Users</a>
-            </li>
-          </ul>
-        </div>
+      <nav className="navbar navbar-expand-lg navbar-light justify-content-center" style={{backgroundColor: "#D2FDFF"}}>
+        <ul className='navbar-nav flex-row'>
+          <li className='nav-item active'>
+            <a className='nav-link' href='#event' style={this.state.viewEvent?{fontWeight: "bolder", color: "Highlight"}:{fontWeight: "bolder"}} onClick={() => this.setState({viewEvent: true})}>Events</a>
+          </li>
+          <li className='nav-item active' style={{marginLeft: "10vw"}}>
+            <a className='nav-link' href='#user' style={!this.state.viewEvent?{fontWeight: "bolder", color: "Highlight"}:{fontWeight: "bolder"}} onClick={() => this.setState({viewEvent: false})}>Users</a>
+          </li>
+        </ul>
       </nav>
-        {this.state.viewEvent ? <EventTable /> : <></>}
+        {this.state.viewEvent ? <EventTable /> : <AccountTable />}
       </div>
     )
   }
@@ -79,7 +77,7 @@ class NavTable extends React.Component{
 
 class EventTable extends React.Component{
   render() {
-    return(<table className="table" style={{backgroundColor: "#F4976C"}}>
+    return(<table id='event' className="table" style={{backgroundColor: "#F4976C"}}>
       <thead>
       <tr>
         <th style={{width: "20%"}}>ID</th>
@@ -104,6 +102,61 @@ class EventTableRow extends React.Component{
         <th>{this.props.data["name"]}</th>
         <th>${this.props.data["price"]}</th>
         <th>{this.props.data["quota"]}</th>
+        <th>
+          <button className='btn' style={{maxHeight:35}}><i class="bi bi-pencil-square"/></button>
+          <button className='btn' style={{maxHeight:35}}><i class="bi bi-trash3"/></button>
+          </th>
+      </tr>
+    )
+  }
+}
+
+class AccountTable extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      accounts: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await fetch('http://localhost:3000/account/all', {
+        method: 'GET'
+      });
+      const data = await response.json();
+      console.log(data);
+      this.setState({ accounts: data });
+    } catch (error) {
+      // Handle any errors from the async operation
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  render() {
+    return(<table id='account' className="table" style={{backgroundColor: "#F4976C"}}>
+      <thead>
+        <tr>
+          <th style={{width: "20%"}}>Username</th>
+          <th style={{width: "20%"}}>Password</th>
+          <th style={{width: "20%"}}>Account Type</th>
+          <th style={{width: "20%"}}>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {this.state.accounts.map((data) => <AccountTableRow data={data}/>)}
+      </tbody>
+  </table>)
+  }
+}
+
+class AccountTableRow extends React.Component{
+  render(){
+    return (
+      <tr>
+        <th>{this.props.data["ID"]}</th>
+        <th>{this.props.data["password"]}</th>
+        <th>{this.props.data["isAdmin"]? 'Admin' : 'User'}</th>
         <th>
           <button className='btn' style={{maxHeight:35}}><i class="bi bi-pencil-square"/></button>
           <button className='btn' style={{maxHeight:35}}><i class="bi bi-trash3"/></button>
